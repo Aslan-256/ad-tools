85a8a5f20e9c20eaaf0f0de95d86a1ee39fff871
3ndry17 <andrearosso2007@gmail.com>
2026-06-23 18:59:50 +0200

    Updated pluto.sh to accept destination path

--- tulip-master/services/pcaps/pluto.sh
+++ tulip-master/services/pcaps/pluto.sh
@@ -1,4 +1,4 @@
 while true; do
-	rsync -rtzvP -e "ssh -i ~/.ssh/toolssh" root@10.60.9.1:/var/log/tcpdump .
-	sleep 60
+    rsync -rtzvP -e "ssh -i ~/.ssh/id_ed25519 -q" root@10.60.5.1:/var/log/tcpdump/ /home/ADTools-main/tulip-master/services/pcaps/tcpdump/3000/
+    sleep 60
 done
